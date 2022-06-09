f096cc2b56ec89a742ab7251482c7a62f7bfbadf
hebaea <hebaelosta98@gmail.com>
2022-06-09 10:03:09 +0200

    add app drawer

--- lib/app/modules/dashboard/views/dashboard_view.dart
+++ lib/app/modules/dashboard/views/dashboard_view.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_phosphor_icons/flutter_phosphor_icons.dart';
 import 'package:get/get.dart';
 import 'package:my_trip/app/core/theme/color_theme.dart';
+import 'package:my_trip/app/global_widgets/default_text.dart';
 import 'package:my_trip/app/modules/chats/views/chats_view.dart';
 import 'package:my_trip/app/modules/favorites/views/favorites_view.dart';
 import 'package:my_trip/app/modules/home/views/home_view.dart';
@@ -16,6 +17,7 @@ class DashboardView extends StatelessWidget {
   Widget build(BuildContext context) {
     return GetBuilder<DashboardController>(builder: (controller) {
       return Scaffold(
+        appBar: AppBar(),
         body: SafeArea(
           child: IndexedStack(
             index: controller.tabIndex,
@@ -48,6 +50,60 @@ class DashboardView extends StatelessWidget {
             _bottomNavigationBarItem(
                 icon: const Icon(PhosphorIcons.user), label: 'حسابي'),
           ],
+        ),
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: [
+              const UserAccountsDrawerHeader(
+                decoration: BoxDecoration(color: AppThemeColors.grayPrimary400),
+                currentAccountPicture: CircleAvatar(
+                  radius: 50.0,
+                  backgroundColor: Colors.white,
+                  child: Icon(
+                    PhosphorIcons.user,
+                    color: AppThemeColors.grayPrimary500,
+                  ),
+                ),
+                accountEmail: Text('hebaelosta98@gmail.com'),
+                accountName: Text('Heba Elosta'),
+              ),
+              ListTile(
+                leading: const Icon(
+                  PhosphorIcons.user,
+                  color: AppThemeColors.grayPrimary500,
+                ),
+                title: DefaultText('بيانات حسابي'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(
+                  PhosphorIcons.article,
+                  color: AppThemeColors.grayPrimary500,
+                ),
+                title: const DefaultText('الاحكام و الشروط'),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(
+                  PhosphorIcons.info,
+                  color: AppThemeColors.grayPrimary500,
+                ),
+                title: const DefaultText(
+                  'معلومات عنا',
+                ),
+                onTap: () {},
+              ),
+              ListTile(
+                leading: const Icon(
+                  PhosphorIcons.sign_out,
+                  color: AppThemeColors.grayPrimary500,
+                ),
+                title: const DefaultText('تسجيل الخروج'),
+                onTap: () {},
+              ),
+            ],
+          ),
         ),
       );
     });
